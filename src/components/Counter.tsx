19cfb8c39f46b4286ea5c7cf7e243e833b3331f8
Blizz <ellamsbliss@gmail.com>
2024-01-11 20:05:05 +0100

    async redux

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,6 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../state/store";
-import { decrement, increment, incrementByAmount } from "../state/counter/counterSlice";
+import {
+  decrement,
+  increment,
+  incrementAsync,
+  incrementByAmount,
+} from "../state/counter/counterSlice";
 
 const Counter = () => {
   const count = useSelector((state: RootState) => state.counter.value);
@@ -10,7 +15,10 @@ const Counter = () => {
       <h2>{count}</h2>
       <div>
         <button onClick={() => dispatch(increment())}>increment</button>
-        <button onClick={() => dispatch(incrementByAmount(10))}>increment by 10</button>
+        <button onClick={() => dispatch(incrementAsync(10))}>increment</button>
+        <button onClick={() => dispatch(incrementByAmount(10))}>
+          increment by 10
+        </button>
         <button onClick={() => dispatch(decrement())}>decrement</button>
       </div>
     </div>
--- src/state/counter/counterSlice.ts
+++ src/state/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 interface CounterState {
   value: number;
@@ -21,6 +21,23 @@ const counterSlice = createSlice({
       state.value += action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(incrementAsync.pending, () => console.log("pending"))
+      .addCase(
+        incrementAsync.fulfilled,
+        (state, action: PayloadAction<number>) => {
+          state.value += action.payload;
+        }
+      );
+  },
 });
+export const incrementAsync = createAsyncThunk(
+  "counter/incrementAsync",
+  async (amount: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return amount;
+  }
+);
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 export default counterSlice.reducer;
